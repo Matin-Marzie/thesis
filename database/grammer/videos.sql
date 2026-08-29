5d097e290a3de0e9d8b30b8e8aa4dd399a4c7371
Matin <matin.marzie5148@gmail.com>
2026-08-29 17:00:40 +0300

    feat(grammer video): Add all 45 Persian Grammer videos from FC LangMedia

--- database/grammer/videos.sql
+++ database/grammer/videos.sql
@@ -1,10 +1,58 @@
 -- Curated video collection shown on the app's Videos screen.
 -- Titles aren't set yet - fill them in as they're decided, or leave NULL
 -- (the app just won't show a caption under the thumbnail for that video).
-
 INSERT INTO grammer_videos (youtube_url, title, learning_language_id) VALUES
-('https://youtu.be/74Np5q55OHE', 'Persian Grammar: Pronouncing and Writing the Persian Alphabet', 3),
-('https://youtu.be/OK2-cgoZK0Q', 'Persian Grammar: Short Vowels a, e, o', 3),
-('https://youtu.be/sfyy12LRGt4', 'Persian Grammar: Long Vowels ā, ī, ū', 3),
-('https://youtu.be/6gb_aUwI9Fs', 'Persian Grammar: Verb "To Be"- Short Forms of "Am, "Is" and "Are"', 3),
-('https://youtu.be/IsAhOF1lu6c', 'Persian Grammar: How to connect Persian characters- Part I', 3);
+
+('https://youtu.be/74Np5q55OHE', 'Pronouncing and Writing the Persian Alphabet', 3),-- Stage 1: Alphabet, Pronunciation & Writing Fundamentals
+('https://youtu.be/OK2-cgoZK0Q', 'Short Vowels a, e, o', 3),
+('https://youtu.be/sfyy12LRGt4', 'Long Vowels ā, ī, ū', 3),
+('https://youtu.be/peugPrgHArk', 'Persian Character Which Share the Same Sound', 3),
+('https://youtu.be/IsAhOF1lu6c', 'How to connect Persian characters- Part I', 3),
+('https://youtu.be/3J7JNO-E1vQ', 'How to connect Persian characters- Part II', 3),
+
+('https://youtu.be/7RhS1s6C4Eo', 'Subjective Pronouns, "I, You, S/He, They ..."', 3), -- Stage 2: Pronouns & Basic Sentence Structures (The Verb "To Be")
+('https://youtu.be/oCkEb4lJ1qE', 'Persian Pronouns, "I, You, They and My, Your, Our"', 3),
+('https://youtu.be/6gb_aUwI9Fs', 'Verb "To Be"- Short Forms of "Am, "Is" and "Are"', 3),
+('https://youtu.be/zhC3weaVRWM', 'Verb "To Be"- Long Forms of "Am, "Is" and "Are"', 3),
+('https://youtu.be/s5qh2AxCoyc', 'Past Tense "To Be''"- "Was/Were"', 3),
+
+('https://youtu.be/92pwZv1Q0k4', 'Pluralization with ''ha'' ها', 3), -- Stage 3: Nouns, Plurals, Ezafe & Descriptors
+('https://youtu.be/WZD2SlOqVwg', 'Pluralization with ''an'' ان', 3),
+('https://youtu.be/cMvewcrh2iQ', 'How to Express Possession in Persian Using Ezafe', 3),
+('https://youtu.be/qFoVx4pUJPQ', 'Where to Place Adjectives in Persian Using Ezafe', 3),
+('https://youtu.be/0zHONUVrmv8', 'Where to Place Names and Titles in Persian Using Ezafe', 3),
+('https://youtu.be/EOkH0sTCHLw', 'Possessive Pronouns, "My,Your, Their..."', 3),
+('https://youtu.be/3YXHbkBybCA', 'Possessive Endings', 3),
+
+('https://youtu.be/zeXHGxH1V8I', 'Cardinal Numbers', 3), -- Stage 4: Numbers, Counting & Quantifiers
+('https://youtu.be/nx6cfC4cLE8', 'Counting Words', 3),
+('https://youtu.be/aJpQXo-hULs', 'Ordinal Numbers', 3),
+
+('https://youtu.be/MaWRj8oG0_8', 'The Definite Article ''ra'' را', 3), -- Stage 5: Objects, Direct Markers & Prepositions
+('https://youtu.be/hdiiZt4uvJ8', 'Objective Pronouns, "Him, Them, Us ..."', 3),
+('https://youtu.be/sOBTr18Va6E', 'Objective Endings', 3),
+('https://youtu.be/1sW3W7gnask', 'Endings with Prepositions or Prepositional Phrases', 3),
+
+('https://youtu.be/TzO-HKzAVAg', 'Infinitives', 3), -- Stage 6: Core Verbs & Simple Tenses
+('https://youtu.be/TjzccZsEtY4', 'Simple and Compound Verbs- "To Do", "To Do Work"', 3),
+('https://youtu.be/JPW-5Ktpg74', 'Simple Past Tense', 3),
+('https://youtu.be/kqpg1LxQqlU', 'Simple Present Tense- "Do" "Am Doing" "Will Do"', 3),
+
+('https://youtu.be/XRVzhkCJ6Gc', 'Forming Yes/No Questions', 3), -- Stage 7: Questions & Interrogatives
+('https://youtu.be/frA66YR4TL0', 'Asking Questions Using "WH" Words', 3),
+('https://youtu.be/39Lsi_MkwcE', 'Asking Questions Using "What"', 3),
+('https://youtu.be/RWS-qIfHknM', 'Asking Questions Using "Where"', 3),
+('https://youtu.be/37lwvOjT7KI', 'Asking Questions Using "When"', 3),
+('https://youtu.be/OIOrnUSj1gY', 'Asking Questions Using "How"', 3),
+('https://youtu.be/twnQCQOSySo', 'Asking Questions Using "How Many"', 3),
+('https://youtu.be/8NSxTrmXsgs', 'Asking Questions Using "How Much"', 3),
+('https://youtu.be/8CKjbdRcX-g', 'Asking Questions Using "Which"', 3),
+
+('https://youtu.be/CTKBC6WQnfc', 'Past Continuous or Past Progressive Tense', 3), -- Stage 8: Intermediate & Advanced Verb Conjugations
+('https://youtu.be/YzbvU6PU-iI', 'Using the Past Continuous/Progressive in Colloquial Speech', 3),
+('https://youtu.be/keV-6IVXnuU', 'Past Participle', 3),
+('https://youtu.be/_rXPpVm4mhM', 'Present Perfect Tense- "I have done"', 3),
+('https://youtu.be/5p26EhOi7YQ', 'Past Perfect or Pluperfect Tense', 3),
+
+('https://youtu.be/cbdFyMJ04u0', 'Real Conditionals', 3), -- Stage 9: Complex Syntax & Conditionals
+('https://youtu.be/rT6yz5yxvh8', 'Unreal Conditionals', 3);
